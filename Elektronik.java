226764163c209c371890e9bc7e0f827a8627a6bc
DerElJoNo <jonoel.klusmann@googlemail.com>
2015-06-28 18:59:33 +0200

    ItemAbsorber now is a subclass of Elektronik

---
+++ Elektronik.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elektronik here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elektronik extends Block
+{
+    /**
+     * Act - do whatever the Elektronik wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    }
+}
--- ItemAbsorber.java
+++ ItemAbsorber.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ItemAbsorber extends Block
+public class ItemAbsorber extends Elektronik
 {
     boolean senkrecht = true;
     
