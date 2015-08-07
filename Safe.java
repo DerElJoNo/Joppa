3480a79879bf46a1f6e1e216526d3bcd9c18220b
DerElJoNo <jonoel.klusmann@googlemail.com>
2015-08-07 19:49:36 +0200

    Standart-Joppa in externer Klasse gespeichert
    So bleibt der Joppa in jedem Level der Selbe, und Inventarinhalte
    bleiben erhalten

---
+++ Safe.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Safe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Safe  
+{
+    public static final Joppa JOPPA = new Joppa();
+    
+    public static Joppa getJoppa()
+    {
+        return JOPPA;
+    }
+}
